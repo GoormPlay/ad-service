d7f163fb631743f28aada5a524df4cd5d6ef220f
no-merit <kewer9024@ajou.ac.kr>
2025-05-08 13:18:03 +0900

    Chore: 설정변경

--- src/main/java/com/goormplay/adservice/ad/exception/Member/AdException.java
+++ src/main/java/com/goormplay/adservice/ad/exception/Member/AdException.java
@@ -1,12 +1,13 @@
 package com.goormplay.adservice.ad.exception.Member;
 
-import com.goormplay.memberservice.member.exception.BaseException;
-import com.goormplay.memberservice.member.exception.BaseExceptionType;
 
-public class MemberException extends BaseException {
+import com.goormplay.adservice.ad.exception.BaseException;
+import com.goormplay.adservice.ad.exception.BaseExceptionType;
+
+public class AdException extends BaseException {
     private final BaseExceptionType exceptionType;
 
-    public MemberException(BaseExceptionType exceptionType) {
+    public AdException(BaseExceptionType exceptionType) {
         this.exceptionType = exceptionType;
     }
 
--- src/main/java/com/goormplay/adservice/ad/exception/Member/AdExceptionType.java
+++ src/main/java/com/goormplay/adservice/ad/exception/Member/AdExceptionType.java
@@ -1,16 +1,16 @@
 package com.goormplay.adservice.ad.exception.Member;
 
-import com.goormplay.memberservice.member.exception.BaseExceptionType;
+import com.goormplay.adservice.ad.exception.BaseExceptionType;
 import org.springframework.http.HttpStatus;
 
-public enum MemberExceptionType implements BaseExceptionType {
+public enum AdExceptionType implements BaseExceptionType {
     NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "회원 정보가 없습니다."),
     WRONG_PASSWORD(HttpStatus.BAD_REQUEST, "잘못된 비밀번호 입니다."),
     ALREADY_EXIST_MEMBER(HttpStatus.BAD_REQUEST, "존재하는 회원 아이디 입니다.");
     private final HttpStatus httpStatus;
     private final String errorMessage;
 
-    MemberExceptionType(HttpStatus httpStatus, String errorMessage) {
+    AdExceptionType(HttpStatus httpStatus, String errorMessage) {
         this.httpStatus = httpStatus;
         this.errorMessage = errorMessage;
     }
