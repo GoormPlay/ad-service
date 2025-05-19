d16cfa3a6aec1478712d401284383c3b9648a9c2
ashenglow <158246901+ashenglow@users.noreply.github.com>
2025-05-19 15:39:14 +0900

    fix: Optimizing API Gateway path (#39)
    클라이언트 → Gateway 요청
    예: /api/public/contents/latest
    1. CustomPreFilter 실행
    X-Original-Path: /api/public/contents/latest 헤더 추가
    2. Gateway RewritePath
    실제 요청 URI는 /contents/latest로 마이크로서비스에 전달
    3. JWT Filter에서 X-Original-Path 확인
    permit-paths 기준으로 허용 여부 체크

--- src/main/java/com/goormplay/adservice/ad/controller/AdController.java
+++ src/main/java/com/goormplay/adservice/ad/controller/AdController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/ad")
+@RequestMapping("/ad")
 @RequiredArgsConstructor
 @Slf4j
 public class AdController {
